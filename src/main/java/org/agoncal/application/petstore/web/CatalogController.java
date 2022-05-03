07fbcabe242cb215e01ff65eda5f096b797d7732
Bruno macedo <bmsilva99@gmail.com>
2022-05-03 12:26:25 -0300

    Adicionando anotações @Setter e @Getter faltantes na refatoração do código.

--- src/main/java/org/agoncal/application/petstore/web/CatalogController.java
+++ src/main/java/org/agoncal/application/petstore/web/CatalogController.java
@@ -1,9 +1,13 @@
 package org.agoncal.application.petstore.web;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import org.agoncal.application.petstore.domain.Item;
 import org.agoncal.application.petstore.domain.Product;
 import org.agoncal.application.petstore.service.CatalogService;
 import org.agoncal.application.petstore.util.Loggable;
+import org.codehaus.jackson.map.JsonSerializer;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
@@ -22,6 +26,8 @@ import java.util.List;
 @SessionScoped
 @Loggable
 @CatchException
+@Setter
+@Getter
 public class CatalogController extends Controller implements Serializable {
 
     // ======================================
@@ -29,6 +35,8 @@ public class CatalogController extends Controller implements Serializable {
     // ======================================
 
     @Inject
+    @Setter(AccessLevel.NONE)
+    @Getter(AccessLevel.NONE)
     private CatalogService catalogService;
 
     private String categoryName;
@@ -38,7 +46,9 @@ public class CatalogController extends Controller implements Serializable {
     private String keyword;
     private Product product;
     private Item item;
+    @Setter(AccessLevel.NONE)
     private List<Product> products;
+    @Setter(AccessLevel.NONE)
     private List<Item> items;
 
     // ======================================
@@ -66,63 +76,4 @@ public class CatalogController extends Controller implements Serializable {
         return "searchresult.faces?keyword=" + keyword + "&faces-redirect=true";
     }
 
-    // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public Item getItem() {
-        return item;
-    }
-
-    public void setItem(Item item) {
-        this.item = item;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public List<Item> getItems() {
-        return items;
-    }
-
-    public String getKeyword() {
-        return keyword;
-    }
-
-    public void setKeyword(String keyword) {
-        this.keyword = keyword;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
-
-    public Long getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(Long itemId) {
-        this.itemId = itemId;
-    }
 }
